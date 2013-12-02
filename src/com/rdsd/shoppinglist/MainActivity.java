9281009d4757d78bb12b680244941e12f4dd3d7d
Tuomas Paulin <tuomas.paulin@gmail.com>
2013-12-02 23:22:28 +0200

    Changed from earlier to activity based, added new CreateShopppingListActivity and changed layout filename to match the default name. Likeabuss. Should work now like it is correct with the streamlined flow of android.

--- src/com/rdsd/shoppinglist/MainActivity.java
+++ src/com/rdsd/shoppinglist/MainActivity.java
@@ -1,7 +1,8 @@
 package com.rdsd.shoppinglist;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 
@@ -20,9 +21,9 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-//	final Button button = (Button) findViewById(R.id.button1);
 	public void goToShoppingList(View view) {
-		setContentView(R.layout.createlist);
+		Intent intent = new Intent(this, CreateShoppingListActivity.class);
+		startActivity(intent);
 	}
 
 }
