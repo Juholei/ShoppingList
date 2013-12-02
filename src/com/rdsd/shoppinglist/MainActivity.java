422c6d4c7b6cf044915919e6e55e3a7f0f631961
Tuomas Paulin <tuomas.paulin@gmail.com>
2013-12-02 15:19:49 +0200

    Added ui classes, one button works already and switches layout! Feelsgoodman

--- src/com/rdsd/shoppinglist/MainActivity.java
+++ src/com/rdsd/shoppinglist/MainActivity.java
@@ -3,13 +3,14 @@ package com.rdsd.shoppinglist;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.startmenu);
 	}
 
 	@Override
@@ -17,6 +18,11 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+//	final Button button = (Button) findViewById(R.id.button1);
+	public void goToShoppingList(View view) {
+		setContentView(R.layout.createlist);
 	}
 
 }
