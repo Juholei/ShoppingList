96b403e7f917217ad3a5996251d0c11c6356deff
Juho Leinonen <juholei@gmail.com>
2013-12-16 11:08:13 +0200

    Small tweak to Subject interface. Added ProductAdapter to add Products to ListView. Did not implement anything yet though.

--- src/com/rdsd/shoppinglist/Interfaces/Subject.java
+++ src/com/rdsd/shoppinglist/Interfaces/Subject.java
@@ -3,4 +3,5 @@ package com.rdsd.shoppinglist.Interfaces;
 public interface Subject {
 	public void addObserver(Observer o);
 	public void removeObserver(Observer o);
+	public void notifyObservers();
 }
