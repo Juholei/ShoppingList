9dfbc5c2179edd9a2c2dfb78c14eb5de63dc40c6
Juho Leinonen <juholei@gmail.com>
2014-01-07 10:13:44 +0200

    BoughtProductsActivity now when the second button in main menu is pressed.

--- src/com/rdsd/shoppinglist/BoughtProductsActivity.java
+++ src/com/rdsd/shoppinglist/BoughtProductsActivity.java
@@ -1,7 +1,10 @@
 package com.rdsd.shoppinglist;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class BoughtProductsActivity extends Activity {
@@ -13,6 +16,12 @@ public class BoughtProductsActivity extends Activity {
 		setContentView(R.layout.activity_bought_products);
 		
 		ListView boughtProductsList = (ListView) findViewById(R.id.boughtProductsList);
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("kakkaa");
+		list.add("ripulia");
+		list.add("paskaa");
+		
+		boughtProductsList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list));
 	}
 
 }
